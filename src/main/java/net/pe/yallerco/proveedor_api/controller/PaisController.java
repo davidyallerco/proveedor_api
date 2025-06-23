1ba6bb5bff8595403ebd0a09d979c7e9ceb7c288
davidyallerco <david.yallerco@gmail.com>
2025-06-22 21:44:16 -0500

    Agregando endpoint para listar header

--- src/main/java/net/pe/yallerco/proveedor_api/controller/PaisController.java
+++ src/main/java/net/pe/yallerco/proveedor_api/controller/PaisController.java
@@ -1,15 +1,18 @@
 package net.pe.yallerco.proveedor_api.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -116,5 +119,19 @@ public class PaisController {
 //		ResponseEntity<CategoriaResponseRest> response = categoriaService.eliminar(id);
 //		return response;
 //	}
+	
+	//Header
+	@GetMapping("/listar-con-headers")
+    public ResponseEntity<Map<String, String>> getAllPaisesWithHeaders(
+            @RequestHeader Map<String, String> headers) {
+
+        // Puedes inspeccionar los headers aquí
+        System.out.println("Headers recibidos en Spring Boot:");
+        headers.forEach((key, value) -> System.out.println(key + " = " + value));
+
+        // Devuelve los headers en la respuesta
+        // Spring Boot automáticamente serializará el Map a JSON
+        return ResponseEntity.ok(headers);
+    }
 
 }
